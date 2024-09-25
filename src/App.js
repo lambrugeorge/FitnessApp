97138f5cc797bb414e3ccfa3b93d5883fe56e01b
George Daniel <lambru_george@yahoo.com>
2024-09-25 23:16:17 +0300

    created components/pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Footer from './components/Footer';
+import './App.css';
+
+
 
 const App = () => {
   return (
-    <div>App</div>
+  <Box width="400px">
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/exercise/:id" element={<ExerciseDetail />} />
+    </Routes>
+    <Footer />
+  </Box>
   )
 }
 
