a6395a017c2b9b9d799d455ce2bfa2501d24216b
George Daniel <lambru_george@yahoo.com>
2024-09-26 21:57:24 +0300

    created Navbar

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import './App.css';
 
 const App = () => {
   return (
-  <Box width="400px">
+  <Box width="400px" sx={{ width: {xl: '1488px' }}} m='auto'>
     <Navbar />
     <Routes>
       <Route path="/" element={<Home />} />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Stack } from '@mui/material';
+import Logo from '../assets/assets/images/Logo.png';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <Stack>
+      <Link to="/">
+        <img
+          src={Logo}
+          alt="logo"
+          style={{ width: '48px', height: '48px', margin: '0 20px' }}
+        />
+      </Link>
+      <Stack>
+        <Link to="/">Home</Link>
+        <a
+          href="#exercises"
+          style={{ textDecoration: 'none', color: '#3A1212', borderBottom: '3px solid #FF2625'}}
+        >
+          Exercises
+        </a>
+      </Stack>
+    </Stack>
+  );
+};
 
-export default Navbar
+export default Navbar;
