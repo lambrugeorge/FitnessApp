32f9c94bcaac5f4d7a173bc638cc7f4135179611
George Daniel <lambru_george@yahoo.com>
2024-09-28 18:29:45 +0300

    finish navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,22 +5,42 @@ import Logo from '../assets/assets/images/Logo.png';
 
 const Navbar = () => {
   return (
-    <Stack>
-      <Link to="/">
+    <Stack 
+      direction="row" 
+      justifyContent="space-around" 
+      sx={{ 
+        gap: { sm: '122px', xs: '40px' }, 
+        mt: { sm: '32px', xs: '20px' },
+        justifyContent: 'none'}} px='20px'>
+
+      <Link to="/"> 
         <img
-          src={Logo}
-          alt="logo"
-          style={{ width: '48px', height: '48px', margin: '0 20px' }}
+          src={Logo} 
+          alt="logo" 
+          style={{ width: '48px', height: '48px', margin: '0 20px' }} 
         />
       </Link>
-      <Stack>
-        <Link to="/">Home</Link>
-        <a
-          href="#exercises"
-          style={{ textDecoration: 'none', color: '#3A1212', borderBottom: '3px solid #FF2625'}}
-        >
-          Exercises
-        </a>
+      <Stack
+        direction='row' 
+        gap="40px" 
+        fontSize="24px" 
+        alignItems="flex-end"
+      >
+        <Stack direction="row" gap="40px" fontSize="24px" alignItems="center">
+          <Link to="/" style={{ textDecoration: 'none', color: '#3A1212' }}>
+            Home
+          </Link>
+          <a
+            href="#exercises"
+            style={{
+              textDecoration: 'none',
+              color: '#3A1212',
+              borderBottom: '3px solid #FF2625',
+            }}
+          >
+            Exercises
+          </a>
+        </Stack>
       </Stack>
     </Stack>
   );
