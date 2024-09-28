3faa2a0df2fcc895fba66f2c2ba6e3543f5bbf25
George Daniel <lambru_george@yahoo.com>
2024-09-28 21:31:05 +0300

    finish HeroBanner component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import React from 'react'
+import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Box } from '@mui/material';
+
+import './App.css';
+import ExerciseDetail from './pages/ExerciseDetail';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
-import ExerciseDetail from './pages/ExerciseDetail';
 import Footer from './components/Footer';
-import './App.css';
 
-
-
-const App = () => {
-  return (
-  <Box width="400px" sx={{ width: {xl: '1488px' }}} m='auto'>
+const App = () => (
+  <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
     <Navbar />
     <Routes>
       <Route path="/" element={<Home />} />
@@ -19,7 +17,6 @@ const App = () => {
     </Routes>
     <Footer />
   </Box>
-  )
-}
+);
 
-export default App
+export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Stack } from '@mui/material';
-import Logo from '../assets/assets/images/Logo.png';
+import Logo from '../assets/images/Logo.png';
 
 const Navbar = () => {
   return (
