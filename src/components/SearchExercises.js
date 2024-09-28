7b8ececa1c7cf6e39de668e1940811138ff2af81
George Daniel <lambru_george@yahoo.com>
2024-09-28 21:35:34 +0300

    create SearchExercises component

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React,{ useEffect, useState} from 'react'
+
+import {Box, Button, Stack, TextField, Typography} from '@mui/material';
 
 function SearchExercises() {
   return (
-    <div>SearchExercises</div>
+    <Stack alighItems="center" mt="37px"
+    justifyContent="center" p="20px">
+
+    </Stack>
   )
 }
 
