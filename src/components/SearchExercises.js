ba66ad55a895ad0cc0967dc141718b18c2d2656e
lambrugeorge <lambru_george@yahoo.com>
2024-10-01 18:43:47 +0300

    create fetch API

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,14 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import { Box, Button, Stack, TextField, Typography } from '@mui/material';
+import { exerciseOptions, fetchData } from '../utils/fetchData';
 
-function SearchExercises() {
+const SearchExercises = () => {
+  const [search, setSearch] = useState('');
+
+  const handleSearch = async () => {
+    if (search) {
+      const exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList');
+      console.log(exercisesData);
+    
+    }
+  };
+
   return (
-    <Stack
-      alignItems="center"
-      mt="37px"
-      justifyContent="center"
-      p="20px"
-    >
+    <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
       <Typography
         fontWeight={700}
         sx={{ fontSize: { lg: '44px', xs: '30px' } }}
@@ -30,11 +36,24 @@ function SearchExercises() {
             width: { lg: '1170px', xs: '350px' },
           }}
           height="76px"
-          value=""
-          onChange={(e) => {}}
+          value={search} 
+          onChange={(e) => setSearch(e.target.value)}
           placeholder="Search Exercises"
           type="text"
         />
+        <Button 
+          sx={{ 
+            bgcolor: '#3C40C6', 
+            color: '#fff', 
+            textTransform: 'none', 
+            height: '56px', 
+            position: 'absolute', 
+            right: '0' 
+          }} 
+          onClick={handleSearch} 
+        >
+          Search
+        </Button>
       </Box>
     </Stack>
   );
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,14 +1,14 @@
 export const exerciseOptions = {
-              method: 'GET',
-              headers: {
-                'x-rapidapi-key': process.env.REACT_APP_RAPID_API_KEY,
-                'x-rapidapi-host': 'exercisedb.p.rapidapi.com'
-              }
-};
-
-export const fetchData = async(url, options) => {
-       const response = await fetch(url, options);
-       const data = await response.json();
-
+       method: 'GET',
+       headers: {
+         'x-rapidapi-key': process.env.REACT_APP_RAPID_API_KEY,
+         'x-rapidapi-host': 'exercisedb.p.rapidapi.com'
+       }
+     };
+     
+     export const fetchData = async (url, options) => {
+       const res = await fetch(url, options);
+       const data = await res.json();
+     
        return data;
-}
+     };
