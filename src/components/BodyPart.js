592eb4e6936051cc115ec98595645decbaf43665
lambrugeorge <lambru_george@yahoo.com>
2024-10-07 21:35:00 +0300

    finish component

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Stack, Typhography } from '@mui/material';
+import { Stack, Typography } from '@mui/material';
 
 import Icon from '../assets/icons/gym.png'
 
@@ -13,26 +13,23 @@ function BodyPart({ item, setBodyPart, bodyPart }) {
     alignItems="center"
     justifyContent="center"
     className="bodyPart-card"
-    sx={
-      bodyPart === item ? {
-        borderTop: '4px solid #ff2625',
-        backgroundColor: '#fff',
-        borderBottomLeftRadius: '20px',
-        width: '270px',
-        height: '280px',
-        cursor: 'pointer', 
-        gap: '47px'
-      } : {
-        background: '#fff',
-        borderBottomLeftRadius: '20px',
-        width: '270px',
-        height: '280px',
-        cursor: 'pointer', 
-        gap: '47px'
-      }
-    }
+    sx={{ 
+      borderTop: bodyPart === item ? '4px solid #ff2625' : '',
+      backgroundColor: '#fff',
+      borderBottomLeftRadius: '20px',
+      width: '270px',
+      height: '280px',
+      cursor: 'pointer', 
+      gap: '47px'
+      }}
+      onClick={() => {
+        setBodyPart(item);
+        window.scrollTo({top: 1800, left: 100, behavior: "smooth"})
+      }}
     >
-      <img src={Icon} alt='dumbbell' styles={{ width: '40px', height: '40px' }} />
+      <img src={Icon} alt='dumbbell' style={{ width: '40px', height: '40px' }} />
+      <Typography fontSize="24px" fontWeight="bold" color="#3A1212"
+      textTransform="capitalize">{item}</Typography>
     </Stack>
   )
 }
