4db810b10c568d9eaf8e08b98811e7c6c1aeaedc
lambrugeorge <lambru_george@yahoo.com>
2024-10-11 23:06:41 +0300

    create ExerciseDetail component

---
+++ src/components/ExerciseVideos.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const ExerciseVideos = () => {
+  return (
+    <div>ExerciseVideos</div>
+  )
+}
+
+export default ExerciseVideos
--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import { useEffect, useState } from "react"
+import React  from 'react'
+import { useParams } from "react-router-dom"
+import { Box } from "@mui/material"
+import { exerciseOptions, fetchData } from "../utils/fetchData"
+import Details from "../components/Details"
+import ExerciseVideos from "../components/ExerciseVideos"
+import { SimilarExercises } from "../components/SimilarExercises"
+
+
 
 const ExerciseDetail = () => {
+const [exerciseDetail, setExerciseDetail] = 
+ useState({});
+ const { id } = useParams();
+
+
+
   return (
-    <div>ExerciseDetail</div>
+    <Box>
+    <Details ExerciseDetail={} />
+    <ExerciseVideos />
+    <SimilarExercises />
+    </Box>
   )
 }
 
