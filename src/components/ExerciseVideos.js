f5198e5e5200a6f7c6b9e1b97e157ea03b619078
George Daniel <lambru_george@yahoo.com>
2024-10-25 00:19:55 +0300

    exercise Videos component

--- src/components/ExerciseVideos.js
+++ src/components/ExerciseVideos.js
@@ -1,8 +1,35 @@
 import React from 'react'
+import {Box, Stack, Typography } from '@mui/material';
 
-export const ExerciseVideos = () => {
+
+const ExerciseVideos = ({ exerciseVideos, name}) => {
   return (
-    <div>ExerciseVideos</div>
+    <Box sx={{ marginTop: {lg: '200px', xs: '20px' }}} p="20px">
+      <Typography variant="h3" mb='33px'>
+      Match <span style={{color: '#ff2625', textTransform: 'capitalize'}}> {name}</span> exercise videos
+      </Typography>
+      <Stack justifyContent="flex-start" flexWrap="wrap" alignItems="center"
+      sx={{
+        flexDirection: { lg: 'row' },
+        gap: { lg: '110px', xs: '0'}
+      }}
+>
+
+  {exerciseVideos?.slice(0, 3).map((item, index) => (
+    <a 
+    key={index}
+    className='exercise-video'
+    href={`https://www.youtube.com/watch?v=${item.video.videoId}`}
+    >
+      
+    </a>
+  ))}
+</Stack>
+
+
+
+
+    </Box>
   )
 }
 
